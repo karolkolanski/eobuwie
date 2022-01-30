0a657fd26d6613ea0fa361c73204f85cdbbea882
Karol Kolanski <gralu86@gmail.com>
2022-01-30 13:23:18 +0100

    Accept button clicked on permision popup

--- test_eobuwie.py
+++ test_eobuwie.py
@@ -1,6 +1,9 @@
 # Import bibliotek
-from selenium import webdriver
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
 
 class TestRegistration(unittest.TestCase):
     def setUp(self):
@@ -15,7 +18,13 @@ class TestRegistration(unittest.TestCase):
         self.driver.quit()
 
     def testInvalidEmail(self):
-        pass
+        driver = self.driver
+        # Kroki:
+        # 1. Kliknij Zarejestruj - otwiera się strona rejestracji
+        accept_btn = driver.find_element(By.XPATH, '//button[@data-testid="permission-popup-accept"]')
+        accept_btn.click()
+        # Kontrolny sleep na końcu -  do usunięcia jak będzie gotowe
+        sleep(3)
 
 # Jeśli uruchamiam ten plik
 if __name__ == "__main__":
