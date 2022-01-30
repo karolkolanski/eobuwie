84dc3c97e3c9404b11b255a1ce75354cc90be8b7
Karol Kolanski <gralu86@gmail.com>
2022-01-30 12:33:07 +0100

    Site now opens

--- test_eobuwie.py
+++ test_eobuwie.py
@@ -6,9 +6,14 @@ class TestRegistration(unittest.TestCase):
     def setUp(self):
         # Przygotowanie testu
         # Otwarta strona https://www.eobuwie.com.pl/
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+        self.driver.get("https://www.eobuwie.com.pl/")
 
     def tearDown(self):
-        pass
+        # Zakończenie testu
+        self.driver.quit()
+
     def testInvalidEmail(self):
         pass
 
